769ebbedcc838bbe9c671ee47030d3257be02d5d
André <andre.gorlin@gmail.com>
2025-03-21 13:19:03 -0300

    Segundo commit

---
+++ lib/app_viwe.dart
@@ -0,0 +1,24 @@
+import 'package:finc/screens/home/viwes/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Expense Tracker",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: const Color(0xFF00B2E7),
+          secondary: const Color(0xFFE064E7),
+          tertiary: const Color(0xFFFF8d6c)
+        )
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
