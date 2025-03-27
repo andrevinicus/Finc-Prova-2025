23783f0696af167c21c52402b7fec31abf3ccd96
André <andre.gorlin@gmail.com>
2025-03-27 16:36:52 -0300

    implementando Banco FireBase

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,6 +1,12 @@
+import 'package:expense_repository/expense_repository.dart';
+import 'package:expenses_tracker/screens/add_expense/blocs/create_expense_bloc/create_expense_bloc.dart';
+import 'package:expenses_tracker/screens/add_expense/blocs/get_categories_bloc/get_categories_bloc.dart';
+import 'package:expenses_tracker/screens/add_expense/views/category_creation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -13,200 +19,202 @@ class _AddExpenseState extends State<AddExpense> {
   TextEditingController expenseController = TextEditingController();
   TextEditingController categoryController = TextEditingController();
   TextEditingController dateController = TextEditingController();
-  DateTime selectedDate = DateTime.now();
+  // DateTime selectDate = DateTime.now();
+  late Expense expense;
+  bool isLoading = false;
 
-@override
+  @override
   void initState() {
     dateController.text = DateFormat('dd/MM/yyyy').format(DateTime.now());
+    expense = Expense.empty;
+    expense.expenseId = const Uuid().v1();
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        appBar: AppBar(
+    return BlocListener<CreateExpenseBloc, CreateExpenseState>(
+      listener: (context, state) {
+        if(state is CreateExpenseSuccess) {
+          Navigator.pop(context, expense);
+        } else if(state is CreateExpenseLoading) {
+          setState(() {
+            isLoading = true;
+          });
+        }
+      },
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
           backgroundColor: Theme.of(context).colorScheme.background,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Adicione Despesas',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 32),
-              SizedBox(
-                //width: MediaQuery.of(context).size.width * 0.5, aumenta e diminue o campo
-                child: TextFormField(
-                  controller: expenseController,
-                  textAlignVertical: TextAlignVertical.center,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    //sinal de dolar no input de texto $
-                    prefixIcon: Icon(
-                      FontAwesomeIcons.dollarSign,
-                      size: 16,
-                      color: Colors.grey.shade500,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
-                controller: categoryController,
-                textAlignVertical: TextAlignVertical.center,
-                readOnly: true,
-                onTap: () {
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.background,
+          ),
+          body: BlocBuilder<GetCategoriesBloc, GetCategoriesState>(
+            builder: (context, state) {
+              if (state is GetCategoriesSuccess) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Add Expenses",
+                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: TextFormField(
+                          controller: expenseController,
+                          textAlignVertical: TextAlignVertical.center,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: const Icon(
+                              FontAwesomeIcons.dollarSign,
+                              size: 16,
+                              color: Colors.grey,
+                            ),
+                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(30), borderSide: BorderSide.none),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      TextFormField(
+                        controller: categoryController,
+                        textAlignVertical: TextAlignVertical.center,
+                        readOnly: true,
+                        onTap: () {},
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: expense.category == Category.empty ? Colors.white : Color(expense.category.color),
+                          prefixIcon: expense.category == Category.empty
+                            ? const Icon(
+                                FontAwesomeIcons.list,
+                                size: 16,
+                                color: Colors.grey,
+                              )
+                            : Image.asset(
+                                'assets/${expense.category.icon}.png',
+                                scale: 2,
+                              ),
+                          suffixIcon: IconButton(
+                            onPressed: () async {
+                              var newCategory = await getCategoryCreation(context);
+                              setState(() {
+                                state.categories.insert(0, newCategory);
+                              });
+                            },
+                            icon: const Icon(
+                              FontAwesomeIcons.plus,
+                              size: 16,
+                              color: Colors.grey,
+                            )
+                          ),
+                          hintText: 'Category',
+                          border: const OutlineInputBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(12)), borderSide: BorderSide.none),
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.vertical(bottom: Radius.circular(12)),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListView.builder(
+                            itemCount: state.categories.length,
+                            itemBuilder: (context, int i) {
+                              return Card(
+                                child: ListTile(
+                                  onTap: () {
+                                    setState(() {
+                                      expense.category = state.categories[i];
+                                      categoryController.text = expense.category.name;
+                                    });
+                                  },
+                                  leading: Image.asset(
+                                    'assets/${state.categories[i].icon}.png',
+                                    scale: 2,
+                                  ),
+                                  title: Text(state.categories[i].name),
+                                  tileColor: Color(state.categories[i].color),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                                ),
+                              );
+                            }
+                          )
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        controller: dateController,
+                        textAlignVertical: TextAlignVertical.center,
+                        readOnly: true,
+                        onTap: () async {
+                          DateTime? newDate = await showDatePicker(context: context, initialDate: expense.date, firstDate: DateTime.now(), lastDate: DateTime.now().add(const Duration(days: 365)));
 
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: const Icon(
-                    FontAwesomeIcons.list,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (ctx) {
-                          return AlertDialog(
-                            title: const Text(
-                                  'Adicionar Categoria',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                            ),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const SizedBox(height: 12,),
-                                TextFormField(                                  
-                                  textAlignVertical: TextAlignVertical.center,
-                                  decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor: Colors.white,
-                                    hintText: 'Nome',//texto que fica dentro do input de texto
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(14),
-                                      borderSide: BorderSide.none,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 12,),
-                                TextFormField(                                  
-                                  textAlignVertical: TextAlignVertical.center,
-                                  decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor: Colors.white,
-                                    hintText: 'Icone',//texto que fica dentro do input de texto
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(14),
-                                      borderSide: BorderSide.none,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 12,),
-                                TextFormField(                                  
-                                  textAlignVertical: TextAlignVertical.center,
-                                  decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor: Colors.white,
-                                    hintText: 'Cor',//texto que fica dentro do input de texto
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(14),
-                                      borderSide: BorderSide.none,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
+                          if (newDate != null) {
+                            setState(() {
+                              dateController.text = DateFormat('dd/MM/yyyy').format(newDate);
+                              // selectDate = newDate;
+                              expense.date = newDate;
+                            });
+                          }
                         },
-                      );
-                    },
-                    icon: Icon(
-                      FontAwesomeIcons.plus,
-                      size: 16,
-                      color: Colors.grey,
-                    ),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: const Icon(
+                            FontAwesomeIcons.clock,
+                            size: 16,
+                            color: Colors.grey,
+                          ),
+                          hintText: 'Date',
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: kToolbarHeight,
+                        child: isLoading
+                          ? const Center(child: CircularProgressIndicator())
+                          : TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  expense.amount = int.parse(expenseController.text);
+                                });
+
+                                context.read<CreateExpenseBloc>().add(CreateExpense(expense));
+                              },
+                              style: TextButton.styleFrom(backgroundColor: Colors.black, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+                              child: const Text(
+                                'Save',
+                                style: TextStyle(fontSize: 22, color: Colors.white),
+                              )
+                            ),
+                      )
+                    ],
                   ),
-                  hintText: ('Categoria'),//texto que fica dentro do input de texto
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
-                controller: dateController,
-                textAlignVertical: TextAlignVertical.center,
-                readOnly: true,
-                onTap: () async {
-                  DateTime? newDate = await showDatePicker(
-                    context: context,
-                    initialDate: selectedDate,
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime.now().add(const Duration(days: 365)),
-                  );
-                  if (newDate != null){
-                    setState(() {
-                      dateController.text = DateFormat('dd/MM/yyyy').format(newDate);
-                      selectedDate = newDate;
-                    });
-                  }
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.calendar, //icone do calendario
-                    size: 16,
-                    color: Colors.grey.shade500,
-                  ),
-                  hintText: ('Data'),//texto que fica dentro do input de texto
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                height: kToolbarHeight,
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 44, 44, 44),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                  ), 
-                  child: Text(
-                    'Save',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.white
-                    ),
-                  )
-                ),
-              ),
-            ],
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
           ),
         ),
       ),
---
+++ packeges/expense_repository/lib/expense_repository.dart
@@ -0,0 +1,6 @@
+library expense_repository;
+
+export 'src/entites/entities.dart';
+export 'src/models/models.dart';
+export 'src/firebase_expense.dart';
+export 'src/expense_repo.dart';
